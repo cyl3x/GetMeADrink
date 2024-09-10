80492c19561e5371892cae861b3a39dca43318b7
THN_Michi <michael.mueller221@gmail.com>
2024-09-10 12:43:49 +0200

    changes

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,20 +2,38 @@
 
 namespace App\Controller;
 
+use App\Repository\OrderRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class OrderController extends AbstractController
 {
     public function __construct(
-        // private readonly OrderRepository $orderRepository,
+        private OrderRepository $orderRepository,
+        private readonly ProductRepository $productRepository,
     ) {
     }
 
     #[Route(path: '/order/{id}', name: 'order', methods: ['GET'])]
-    public function order(): Response
+    public function order(string $id): Response
     {
-        return $this->render('order/index.html.twig');
+        $openOrder = $this->orderRepository->findOneBy(['table' => $id, 'status' => 1]);
+        $products = $this->productRepository->findAll();
+
+        return $this->render('order/index.html.twig', [
+            'products' => $products,
+        ]);
     }
+
+    #[Route(path: '/product/select', name: 'product.select', methods: ['POST'])]
+    public function addToOrder(Request $request)
+    {
+        $productId = $request->request->get('product_id');
+
+
+    }
+
 }
