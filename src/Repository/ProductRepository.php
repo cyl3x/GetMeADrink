1f9451d241ab3e1e5b9f820971afd756ef468c7e
cyl3x <cyl3x@posteo.de>
2024-09-10 10:34:04 +0200

    Added product entity

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -2,18 +2,18 @@
 
 namespace App\Repository;
 
-use App\Entity\TableEntity;
+use App\Entity\ProductEntity;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<TableEntity>
+ * @extends ServiceEntityRepository<ProductEntity>
  */
-class TableRepository extends ServiceEntityRepository
+class ProductRepository extends ServiceEntityRepository
 {
     public function __construct(
         ManagerRegistry $registry,
     ) {
-        parent::__construct($registry, TableEntity::class);
+        parent::__construct($registry, ProductEntity::class);
     }
 }
