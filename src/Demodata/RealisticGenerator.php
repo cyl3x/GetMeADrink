758a21527d73596011429933002a6e038773b228
frakes <dev.leiermannjonathan@gmail.com>
2024-11-08 11:19:18 +0100

    Demodata update

--- src/Demodata/RealisticGenerator.php
+++ src/Demodata/RealisticGenerator.php
@@ -39,6 +39,9 @@ class RealisticGenerator implements DemodataGeneratorInterface
 
         $products = \array_map(
             function (array $array) use ($em) {
+
+
+
                 $product = (new ProductEntity())
                 ->setName($array[0])
                 ->addCategory($array[2])
@@ -46,58 +49,69 @@ class RealisticGenerator implements DemodataGeneratorInterface
                 ->setPrice($array[3])
                 ->setVat(0.07);
 
+                if ($array[4] !== null){
+                    $imagePath = __DIR__ . '/Images/' . $array[4];
+                    $image = file_get_contents($imagePath, true);
+                    $product->setImage($image);
+                }
+
                 $array[2]->addProduct($product);
 
                 $em->persist($product);
                 return $product;
             },
             [
-                ['Coca Cola', $variants[6], $categories[3], 1.49],
-                ['Sprite', $variants[6], $categories[3], 1.49],
-                ['Sprite', $variants[7], $categories[3], 1.99],
-                ['Sprite Zero', $variants[6], $categories[3], 1.49],
-                ['Sprite Zero', $variants[7], $categories[3], 1.99],
-                ['Fanta', $variants[6], $categories[3], 1.49],
-                ['Fanta', $variants[7], $categories[3], 1.99],
-                ['Mezzo Mix', $variants[6], $categories[3], 1.49],
-                ['Mezzo Mix', $variants[7], $categories[3], 1.99],
-                ['Paulaner Spezi', $variants[6], $categories[3], 1.49],
-                ['Paulaner Spezi', $variants[7], $categories[3], 1.99],
-                ['Dr.Pepper', $variants[6], $categories[3], 1.49],
-                ['Dr.Pepper', $variants[7], $categories[3], 1.99],
-                ['Schwarzer Kaffee', $variants[4], $categories[0], 2.50],
-                ['Espresso', $variants[0], $categories[0], 2.50],
-                ['Americano', $variants[4], $categories[0], 3.00],
-                ['Cappuccino', $variants[3], $categories[0], 3.00],
-                ['Latte Macciato', $variants[5], $categories[0], 3.50],
-                ['Heiße Schokolade', $variants[4], $categories[0], 3.00],
-                ['Grüner Tee', $variants[4], $categories[0], 2.50],
-                ['Gerolsteiner Sprudel', $variants[7], $categories[1], 0.99],
-                ['Gerolsteiner Medium', $variants[7], $categories[1], 0.99],
-                ['Gerolsteiner Naturell', $variants[7], $categories[1], 0.99],
-                ['Pfanner Pfirsich Eistee', $variants[6], $categories[1], 1.49],
-                ['Orangensaft', $variants[6], $categories[1], 1.49],
-                ['Apfelsaft', $variants[6], $categories[1], 1.49],
-                ['Gin Tonic', $variants[4], $categories[2], 6.50],
-                ['Whisky Cola', $variants[4], $categories[2], 7.00],
-                ['Mojito', $variants[5], $categories[2], 7.50],
-                ['Caipirinha', $variants[5], $categories[2], 7.50],
-                ['Aperol Spritz', $variants[5], $categories[2], 6.80],
-                ['Margarita', $variants[4], $categories[2], 8.00],
-                ['Old Fashioned', $variants[3], $categories[2], 9.00],
-                ['Martini', $variants[2], $categories[2], 7.50],
-                ['Piña Colada', $variants[5], $categories[2], 8.50],
-                ['Tequila Sunrise', $variants[4], $categories[2], 7.00],
-                ['Bloody Mary', $variants[4], $categories[2], 7.50],
-                ['Cosmopolitan', $variants[2], $categories[2], 8.00],
-                ['Mai Tai', $variants[4], $categories[2], 8.50],
-                ['Long Island Iced Tea', $variants[5], $categories[2], 9.00],
-                ['Gin Fizz', $variants[4], $categories[2], 7.00],
-                ['Negroni', $variants[3], $categories[2], 8.50],
-                ['Whiskey Sour', $variants[3], $categories[2], 7.50],
-                ['Rum Cola', $variants[4], $categories[2], 6.50],
-                ['Campari Soda', $variants[3], $categories[2], 6.00],
-                ['Bellini', $variants[2], $categories[2], 7.00]
+                ['Coca Cola', $variants[6], $categories[3], 1.49,"Coca_Cola.png"],
+                ['Coca Cola', $variants[7], $categories[3], 1.99, "Coca_Cola.png"],
+                ['Coca Cola Light', $variants[6], $categories[3], 1.49,"Coca_Cola_Light.png"],
+                ['Coca Cola Light', $variants[7], $categories[3], 1.99, "Coca_Cola_Light.png"],
+                ['Coca Cola Zero', $variants[6], $categories[3], 1.49, "Coca_Cola_Zero.png"],
+                ['Coca Cola Zero', $variants[7], $categories[3], 1.99, "Coca_Cola_Zero.png"],
+                ['Sprite', $variants[6], $categories[3], 1.49, null],
+                ['Sprite', $variants[7], $categories[3], 1.99, null],
+                ['Sprite Zero', $variants[6], $categories[3], 1.49, null],
+                ['Sprite Zero', $variants[7], $categories[3], 1.99, null],
+                ['Fanta', $variants[6], $categories[3], 1.49, null],
+                ['Fanta', $variants[7], $categories[3], 1.99, null],
+                ['Mezzo Mix', $variants[6], $categories[3], 1.49, null],
+                ['Mezzo Mix', $variants[7], $categories[3], 1.99, null],
+                ['Paulaner Spezi', $variants[6], $categories[3], 1.49, null],
+                ['Paulaner Spezi', $variants[7], $categories[3], 1.99, null],
+                ['Dr.Pepper', $variants[6], $categories[3], 1.49, null],
+                ['Dr.Pepper', $variants[7], $categories[3], 1.99, null],
+                ['Schwarzer Kaffee', $variants[4], $categories[0], 2.50, null],
+                ['Espresso', $variants[0], $categories[0], 2.50, null],
+                ['Americano', $variants[4], $categories[0], 3.00, null],
+                ['Cappuccino', $variants[3], $categories[0], 3.00, null],
+                ['Latte Macciato', $variants[5], $categories[0], 3.50, null],
+                ['Heiße Schokolade', $variants[4], $categories[0], 3.00, null],
+                ['Grüner Tee', $variants[4], $categories[0], 2.50, null],
+                ['Gerolsteiner Sprudel', $variants[7], $categories[1], 0.99, null],
+                ['Gerolsteiner Medium', $variants[7], $categories[1], 0.99, null],
+                ['Gerolsteiner Naturell', $variants[7], $categories[1], 0.99, null],
+                ['Pfanner Pfirsich Eistee', $variants[6], $categories[1], 1.49, null],
+                ['Orangensaft', $variants[6], $categories[1], 1.49, null],
+                ['Apfelsaft', $variants[6], $categories[1], 1.49, null],
+                ['Gin Tonic', $variants[4], $categories[2], 6.50, null],
+                ['Whisky Cola', $variants[4], $categories[2], 7.00, null],
+                ['Mojito', $variants[5], $categories[2], 7.50, null],
+                ['Caipirinha', $variants[5], $categories[2], 7.50, null],
+                ['Aperol Spritz', $variants[5], $categories[2], 6.80, null],
+                ['Margarita', $variants[4], $categories[2], 8.00, null],
+                ['Old Fashioned', $variants[3], $categories[2], 9.00, null],
+                ['Martini', $variants[2], $categories[2], 7.50, null],
+                ['Piña Colada', $variants[5], $categories[2], 8.50, null],
+                ['Tequila Sunrise', $variants[4], $categories[2], 7.00, null],
+                ['Bloody Mary', $variants[4], $categories[2], 7.50, null],
+                ['Cosmopolitan', $variants[2], $categories[2], 8.00, null],
+                ['Mai Tai', $variants[4], $categories[2], 8.50, null],
+                ['Long Island Iced Tea', $variants[5], $categories[2], 9.00, null],
+                ['Gin Fizz', $variants[4], $categories[2], 7.00, null],
+                ['Negroni', $variants[3], $categories[2], 8.50, null],
+                ['Whiskey Sour', $variants[3], $categories[2], 7.50, null],
+                ['Rum Cola', $variants[4], $categories[2], 6.50, null],
+                ['Campari Soda', $variants[3], $categories[2], 6.00, null],
+                ['Bellini', $variants[2], $categories[2], 7.00, null]
             ],
         );
 
