5622ae23cada5a57259208f3c448c01bab525170
cyl3x <cyl3x@posteo.de>
2024-11-22 10:43:11 +0100

    Added table detail page

--- frontend/pages/admin/table/AdminTablesListingPage.vue
+++ frontend/pages/admin/table/AdminTablesListingPage.vue
@@ -1,17 +1,66 @@
 <template>
 <div class='w-100'>
     <table class='table table-striped'>
+        <thead>
+            <tr>
+                <th scope='col'>
+                    #
+                </th>
+                <th scope='col'>
+                    Tisch
+                </th>
+                <th scope='col'>
+                    Erstellt am
+                </th>
+                <th scope='col'>
+                    Aktionen
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr
+                v-for='table in tables'
+                :key='table.id'
+            >
+                <th scope='row'>{{ table.id }}</th>
+                <td>{{ table.id }}</td>
+                <td>{{ formatDate(table.createdAt) }}</td>
+                <td>
+                    <button
+                        class='btn btn-primary btn-sm'
+                        @click='router.push({ name: "admin.table", params: { id: table.id } })'
+                    >
+                        Bearbeiten
+                    </button>
+                </td>
+            </tr>
+        </tbody>
     </table>
 </div>
 </template>
 
 <script setup lang='ts'>
-import { onMounted, onUnmounted } from 'vue';
+import { TableService } from '@/services';
+import { onMounted, onUnmounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const tables = ref<Entity.Table[]>([]);
+const router = useRouter();
 
 onMounted(() => { window.addEventListener('admin::create', create); });
 onUnmounted(() => { window.removeEventListener('admin::create', create); });
 
 function create() {
-    console.log('create new table');
+    router.push({ name: 'admin.table' });
 }
+
+async function fetchTables() {
+    tables.value = await TableService.getTables();
+}
+
+function formatDate(date: string) {
+    return new Date(date).toLocaleString('de-DE');
+}
+
+fetchTables();
 </script>
--- frontend/services/table-service.ts
+++ frontend/services/table-service.ts
@@ -3,3 +3,7 @@ import Api from './api';
 export function getTables() {
     return Api.get<Entity.Table[]>('/tables');
 }
+
+export function getTable(id: number) {
+    return Api.get<Entity.Table>(`/table/${id}`);
+}
